f5515e4065eecfd4207efd42975ee35101cedba2
bshk102 <xartem.kx@gmail.com>
2022-11-07 20:10:14 +0500

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,42 @@
+const fs = require('fs')
+const path = require('path')
 
+const copyDir = () => {
+  fs.mkdir(path.resolve(__dirname, 'files-copy'), err => {
+    if (err) {
+      fs.readdir(path.resolve(__dirname, 'files-copy'), (err, data) => {
+        if (err) {
+          throw new Error(err)
+        }
+
+        data.forEach(file => {
+          fs.unlink(path.resolve(__dirname, 'files-copy', file), err => {
+            if (err) {
+              throw new Error(err)
+            }
+          })
+        })
+      })
+    }
+
+    fs.readdir(path.resolve(__dirname, 'files'), (err, data) => {
+      if (err) {
+        throw new Error(err)
+      }
+
+      data.forEach(file => {
+        fs.copyFile(
+          path.resolve(__dirname, 'files', file),
+          path.resolve(__dirname, 'files-copy', file),
+          err => {
+            if (err) {
+              throw new Error(err)
+            }
+          }
+        )
+      })
+    })
+  })
+}
+
+copyDir()
