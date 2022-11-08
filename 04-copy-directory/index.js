3a7eefa712a7d703aef61d279b30b5ac19c84c3f
Artem Kasimov <xartem.kx@gmail.com>
2022-11-08 16:48:25 +0500

    fix: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,41 +2,38 @@ const fs = require('fs')
 const path = require('path')
 
 const copyDir = () => {
-  fs.mkdir(path.resolve(__dirname, 'files-copy'), err => {
-    if (err) {
-      fs.readdir(path.resolve(__dirname, 'files-copy'), (err, data) => {
+  fs.promises
+    .mkdir(path.resolve(__dirname, 'files-copy'))
+    .then(() => {
+      fs.readdir(path.resolve(__dirname, 'files'), (err, data) => {
         if (err) {
           throw new Error(err)
         }
 
         data.forEach(file => {
-          fs.unlink(path.resolve(__dirname, 'files-copy', file), err => {
-            if (err) {
-              throw new Error(err)
+          fs.copyFile(
+            path.resolve(__dirname, 'files', file),
+            path.resolve(__dirname, 'files-copy', file),
+            err => {
+              if (err) {
+                throw new Error(err)
+              }
             }
-          })
+          )
         })
       })
-    }
-
-    fs.readdir(path.resolve(__dirname, 'files'), (err, data) => {
-      if (err) {
-        throw new Error(err)
-      }
-
-      data.forEach(file => {
-        fs.copyFile(
-          path.resolve(__dirname, 'files', file),
-          path.resolve(__dirname, 'files-copy', file),
-          err => {
-            if (err) {
-              throw new Error(err)
-            }
-          }
-        )
-      })
     })
-  })
+    .catch(() => {
+      fs.promises
+        .readdir(path.resolve(__dirname, 'files-copy'))
+        .then(data => {
+          data.forEach(file => {
+            fs.promises.unlink(path.resolve(__dirname, 'files-copy', file))
+          })
+        })
+        .then(() => fs.promises.rmdir(path.resolve(__dirname, 'files-copy')))
+        .then(() => copyDir())
+    })
 }
 
 copyDir()
