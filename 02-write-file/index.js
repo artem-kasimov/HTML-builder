c6d4b6f66d0e4f90b40e920d1cfb9aaa189a5138
Artem Kasimov <xartem.kx@gmail.com>
2022-11-08 11:23:34 +0500

    fix: 02-write-file on ctrl+c message

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,13 +2,18 @@ const fs = require('fs')
 const path = require('path')
 const output = fs.createWriteStream(path.resolve(__dirname, 'text.txt'))
 
+const end = () => {
+  process.stdout.write('Goodbye!')
+  process.exit()
+}
+
 process.stdout.write('Write your text: \n')
 
 process.stdin.on('data', data => {
   if (data.toString().trim() === 'exit') {
-    process.exit()
+    end()
   }
   output.write(data)
 })
 
-process.on('exit', () => process.stdout.write('Goodbye!'))
+process.on('SIGINT', end)
