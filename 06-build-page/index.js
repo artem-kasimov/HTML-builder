86884684e43fede9f7ab7597e28fce2d1d7068ae
Artem Kasimov <xartem.kx@gmail.com>
2022-11-09 14:16:18 +0500

    feat: 06-copy assets

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -3,14 +3,43 @@ const fsPromises = require('fs/promises')
 const path = require('path')
 
 const createDir = name => {
-  fsPromises.mkdir(path.resolve(__dirname, name)).catch(err => {
-    if (err.code === 'EEXIST') {
-      fsPromises
-        .rm(path.resolve(__dirname, name), {
-          recursive: true,
+  fsPromises
+    .mkdir(path.resolve(__dirname, name))
+    .then(() => {
+      copyDir(
+        'assets',
+        path.resolve(__dirname, 'assets'),
+        path.resolve(__dirname, 'project-dist')
+      )
+    })
+    .catch(err => {
+      if (err.code === 'EEXIST') {
+        fsPromises
+          .rm(path.resolve(__dirname, name), {
+            recursive: true,
+          })
+          .then(() => createDir(name))
+      }
+    })
+}
+
+const copyDir = (dirName, dirPath, destDir) => {
+  const destDirPath = path.resolve(destDir, dirName)
+  fsPromises.mkdir(destDirPath).then(() => {
+    fsPromises.readdir(dirPath).then(dirItems => {
+      dirItems.forEach(item => {
+        fsPromises.stat(path.resolve(dirPath, item)).then(stat => {
+          if (stat.isDirectory()) {
+            copyDir(item, path.resolve(dirPath, item), destDirPath)
+          } else if (stat.isFile()) {
+            fsPromises.copyFile(
+              path.resolve(dirPath, item),
+              path.resolve(destDirPath, item)
+            )
+          }
         })
-        .then(() => createDir(name))
-    }
+      })
+    })
   })
 }
 
