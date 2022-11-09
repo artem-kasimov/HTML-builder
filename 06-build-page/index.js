409f8675dfe383c96c9a05cc760c6a50db60c2a8
Artem Kasimov <xartem.kx@gmail.com>
2022-11-09 17:12:29 +0500

    feat: 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -12,6 +12,52 @@ const createDir = name => {
         path.resolve(__dirname, 'project-dist')
       )
     })
+    .then(() => {
+      mergeStyles('styles', 'project-dist', 'style.css')
+    })
+    .then(async () => {
+      let components
+      await fsPromises
+        .readdir(path.resolve(__dirname, 'components'))
+        .then(fileNames => (components = fileNames.map(fn => fn.slice(0, -5))))
+      return components
+    })
+    .then(async components => {
+      let strings
+      await fsPromises
+        .readFile(path.resolve(__dirname, 'template.html'), 'utf-8')
+        .then(data => (strings = data.split('\n')))
+
+      let numsOfStrs = []
+      let namesOfComponents = []
+
+      strings.forEach((str, i) => {
+        for (let component of components) {
+          if (str.trim() === `{{${component}}}`) {
+            numsOfStrs.push(i)
+            namesOfComponents.push(component + '.html')
+          }
+        }
+      })
+
+      for (let i = 0; i < namesOfComponents.length; i++) {
+        await fsPromises
+          .readFile(
+            path.resolve(__dirname, 'components', namesOfComponents[i]),
+            'utf-8'
+          )
+          .then(fileContent => {
+            strings[numsOfStrs[i]] = fileContent
+          })
+      }
+      return strings
+    })
+    .then(strings => {
+      fsPromises.writeFile(
+        path.resolve(__dirname, 'project-dist', 'index.html'),
+        strings.join('\n')
+      )
+    })
     .catch(err => {
       if (err.code === 'EEXIST') {
         fsPromises
@@ -41,6 +87,37 @@ const copyDir = (dirName, dirPath, destDir) => {
       })
     })
   })
+}
+
+const mergeStyles = (srcDir, destDir, outputFile) => {
+  fs.readdir(
+    path.resolve(__dirname, srcDir),
+    { withFileTypes: true },
+    (err, data) => {
+      if (err) {
+        throw new Error(err)
+      }
+
+      const styles = fs.createWriteStream(
+        path.resolve(__dirname, destDir, outputFile)
+      )
+
+      data.forEach(file => {
+        if (
+          file.isFile() &&
+          path.extname(path.resolve(__dirname, srcDir, file.name)) === '.css'
+        ) {
+          const readStream = fs.createReadStream(
+            path.resolve(__dirname, srcDir, file.name)
+          )
+
+          readStream.on('data', data => {
+            styles.write(data.toString() + '\n')
+          })
+        }
+      })
+    }
+  )
 }
 
 createDir('project-dist')
